520e550e8b50ca3f4b61f75490a5c10858b33319
ZunF1x <75583421+ZunF1x@users.noreply.github.com>
2023-07-18 19:52:25 +0200

    Sum

--- src/main/java/fr/mc2d/authapi/utils/HttpUtil.java
+++ src/main/java/fr/mc2d/authapi/utils/HttpUtil.java
@@ -1,7 +1,6 @@
 package fr.mc2d.authapi.utils;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
